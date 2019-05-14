9331d07c53313bdb326576ddc1277de35a8d7e7b
mtarutin <mtarutin@diasoft.ru>
2019-05-14 12:41:03 +0500

    MainActivity and icons

--- app/src/main/java/com/example/orcbuilder/MainActivity.java
+++ app/src/main/java/com/example/orcbuilder/MainActivity.java
@@ -8,35 +8,11 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    textMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    textMessage.setText(R.string.title_gear);
-                    return true;
-                case R.id.navigation_notifications:
-                    textMessage.setText(R.string.title_talents);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        textMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
     }
 
 }
